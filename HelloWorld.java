ec8676536bba9eb7c3ac7da61b223753431202b6
Alan Ingersoll <ingersoll.alan@gmail.com>
2021-08-25 13:22:28 -0700

    Tested out Hello World for Github

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package package1;
+
+/**
+ * @author Alan Ingersoll
+ * @description Main Method prints "Hello World" to the console
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 * main method prints "Hello World" to the console
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
